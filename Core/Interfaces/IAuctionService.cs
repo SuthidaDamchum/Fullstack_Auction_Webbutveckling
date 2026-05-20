2450b63ba812a8e9a55af73340883fe916660b11
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-05-20 14:40:46 +0200

    Added Bid Interface

--- Core/Interfaces/IAuctionService.cs
+++ Core/Interfaces/IAuctionService.cs
@@ -6,5 +6,6 @@ namespace Core.Interfaces
     public interface IAuctionService
     {
         Task<IEnumerable<AuctionDTO>> GetAllAsync(string? search);
+
     }
 }
---
+++ Core/Interfaces/IBidService.cs
@@ -0,0 +1,14 @@
+﻿using Domain.DTOs;
+using Domain.Entities;
+
+namespace Core.Interfaces
+{
+    public interface IBidService
+    {
+        Task<BidDTO> AddBid(Bid bid);
+        Task<IEnumerable<BidDTO>> GetBidsByAuctionId(int id);
+        Task<BidDTO> GetWinnerBid(int auctionId);
+        Task<BidDTO> GetHighestBid(int auctionId);
+        Task<IEnumerable<BidDTO>> DeleteBid(int Id);
+    }
+}
