790e9dcbfa28a24ccf58e4b01fbf12e670e3b044
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-05-20 14:41:26 +0200

    Added bid function in repo

--- Data/Repositories/BidRepository.cs
+++ Data/Repositories/BidRepository.cs
@@ -13,29 +13,64 @@ namespace Data.Repositories
             _context = context;
         }
 
-        public Task<Bid> AddBid(Bid bid)
+        public async Task<Bid> AddBid(Bid bid)
         {
-            throw new NotImplementedException();
+            bid.BidTime = DateTime.Now;
+            _context.Bids.Add(bid);
+            await _context.SaveChangesAsync();
+            return bid;
         }
 
-        public Task<IEnumerable<Bid>> DeleteBid(int auctionId)
+        public async Task<IEnumerable<Bid>> DeleteBid(int bidId)
         {
-            throw new NotImplementedException();
+            //Hitta bid
+            var bid = await _context.Bids.FindAsync(bidId);
+
+            if (bid == null) return null;
+
+            //Finns det högre bud?
+            var newBidExists = await _context.Bids
+                 .AnyAsync(b => b.AuctionId == bid.AuctionId
+                             && b.BidTime > bid.BidTime);
+            //Finns nya budet?
+            if (newBidExists) return null;
+
+            //Tabort
+            int auctionId = bid.AuctionId;
+            _context.Bids.Remove(bid);
+            await _context.SaveChangesAsync();
+
+            //Returnera 
+            return await _context.Bids
+                .Where(b => b.AuctionId == auctionId)
+                .OrderByDescending(b => b.BidTime)
+                .ToListAsync();
         }
 
-        public Task<IEnumerable<Bid>> GetBidsByAuctionId(int id)
+        public async Task<IEnumerable<Bid>> GetBidsByAuctionId(int id)
         {
-            throw new NotImplementedException();
+            return await _context.Bids
+                //Hämta alla bud som tillhör denna aktion
+                .Where(b => b.AuctionId == id)
+                //Sortera högsta bud
+                .OrderByDescending(b => b.Amount)
+                .ToListAsync();
         }
 
-        public Task<Bid> GetHighestBid()
+        public async Task<Bid> GetHighestBid(int auctionId)
         {
-            throw new NotImplementedException();
+            return await _context.Bids
+                .Where(b => b.AuctionId == auctionId)
+                .OrderByDescending(b => b.Amount)
+                .FirstOrDefaultAsync();
         }
 
-        public Task<Bid> GetWinnerBid()
+        public async Task<Bid> GetWinnerBid(int auctionId)
         {
-            throw new NotImplementedException();
+            return await _context.Bids
+                .Where(b => b.AuctionId == auctionId)
+                  .OrderByDescending(b => b.Amount)
+                .FirstOrDefaultAsync();
         }
     }
 }
