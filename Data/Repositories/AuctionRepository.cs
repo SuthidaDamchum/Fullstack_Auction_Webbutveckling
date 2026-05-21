e73d2f0d3f7797b51fdcf85f3a8435d0f3e0f666
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-05-21 13:17:16 +0200

    Implemented functions in AuctionRepos

--- Data/Repositories/AuctionRepository.cs
+++ Data/Repositories/AuctionRepository.cs
@@ -1,5 +1,7 @@
 ﻿using Data.Interfaces;
+using Domain.DTOs;
 using Domain.Entities;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
 
 namespace Data.Repositories
@@ -13,7 +15,25 @@ namespace Data.Repositories
             _context = context;
         }
 
-        public async Task<IEnumerable<Auction>> GetAllAsync(string? search)
+        public async Task<Auction> CreateAuction(Auction auction)
+        {
+            _context.Auctions.Add(auction);
+            await _context.SaveChangesAsync();
+            return auction;
+        }
+
+        public async Task DeactivateAuction(int id)
+        {
+            var closeAuction = await _context.Auctions.FindAsync(id);
+
+            if (closeAuction == null) throw new Exception("Could not find the auction!");
+
+            closeAuction.IsActive = false;
+
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<IEnumerable<Auction>> GetAllAuctionsAsync(string? search)
         {
             var query = _context.Auctions
               .Include(a => a.CreatedBy)
@@ -24,12 +44,48 @@ namespace Data.Repositories
                 query = query.Where(auction => auction.Title.Contains(search));
             }
 
-            return await query.ToListAsync<Auction>();
+            return await query.ToListAsync();
         }
 
         public async Task<Auction> GetAuctionById(int id)
         {
             return await _context.Auctions.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<Auction>> SearchClosedAuctions(string? title)
+        {
+            var query = _context.Auctions
+                .Where(q => q.EndDate < DateTime.UtcNow);
+
+            if (!string.IsNullOrEmpty(title))
+            {
+                query = query.Where(auctions => auctions.Title.Contains(title));
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task<Auction> UpdateAuction(int id, UpdateAuctionDTO updateAuctionDTO)
+        {
+            //Find ID
+            var auction = await _context.Auctions.FindAsync(id);
+             
+            if (auction == null) throw new Exception("Auction not found");
+
+            //if can change, only 
+            auction.Title = updateAuctionDTO.Title;
+            auction.Description = updateAuctionDTO.Description;
+            auction.EndDate = updateAuctionDTO.EndDate;
+
+            //Check bud before price can be chnaged
+            var hasBids = await _context.Bids
+                .AnyAsync(b => b.AuctionId == id);
+
+            if (!hasBids)
+                auction.Price = updateAuctionDTO.Price;
+
+            await _context.SaveChangesAsync();
+
+            return auction;
         }
     }
 }
--- Domain/Mappings/AuctionMapper.cs
+++ Domain/Mappings/AuctionMapper.cs
@@ -1,5 +1,4 @@
-﻿using System.Runtime.CompilerServices;
-using Domain.DTOs;
+﻿using Domain.DTOs;
 using Domain.Entities;
 
 namespace Domain.Mappings
@@ -9,7 +8,7 @@ namespace Domain.Mappings
         public static AuctionDTO ToDto(this Auction auction) => new AuctionDTO
         {
             Id = auction.Id,
-            UserId = auction.UserId,               
+            UserId = auction.UserId,
             CreatedBy = auction.CreatedBy?.Name,
             Title = auction.Title,
             Description = auction.Description,
@@ -17,6 +16,17 @@ namespace Domain.Mappings
             StartDate = auction.StartDate,
             EndDate = auction.EndDate,
             IsActive = auction.IsActive
+        };
+
+        public static Auction ToEntity(this CreateAuctionDTO dto) => new Auction
+        {
+            Title = dto.Title,
+            Description = dto.Description,
+            Price = dto.Price,
+            StartDate = dto.StartDate,
+            EndDate = dto.EndDate,
+            IsActive = true,
+            UserId = dto.UserId
         };
     }
 }
