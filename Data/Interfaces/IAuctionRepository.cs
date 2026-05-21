c41096e0428854062e47f527439847d24c914ae9
SuthidaDamchum <suthida.damchum@hotmail.com>
2026-05-21 12:35:17 +0200

    Added functions to IAuctionRepos to fulfill VG requirements

--- Data/Interfaces/IAuctionRepository.cs
+++ Data/Interfaces/IAuctionRepository.cs
@@ -1,10 +1,15 @@
-﻿using Domain.Entities;
+﻿using Domain.DTOs;
+using Domain.Entities;
 
 namespace Data.Interfaces
 {
     public interface IAuctionRepository
     {
-        Task<IEnumerable<Auction>> GetAllAsync(string? search);
+        Task<IEnumerable<Auction>> GetAllAuctionsAsync(string? search);
         Task <Auction> GetAuctionById(int id);
+        Task<Auction> CreateAuction(Auction auction);
+        Task<Auction> UpdateAuction(int id, UpdateAuctionDTO updateAuctionDTO );
+        Task<IEnumerable<Auction>> SearchClosedAuctions(string? title);
+        Task DeactivateAuction(int id);
     }
 }
